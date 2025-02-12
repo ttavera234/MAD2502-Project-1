2ba32850155758cc39e4a868ec8acb3c74d3d165
lyangelina <angelinalyy27@gmail.com>
2025-02-11 23:25:26 -0500

    added docstring to get_cartesian_coordinates function

--- barycentric.py
+++ barycentric.py
@@ -1,5 +1,13 @@
 import numpy as np
-def get_cartesian_coordinates(triangle_coordinates: np.ndarray[[2, 3]], barycentric_coordinates: np.ndarray[[1,3]]) -> np.ndarray:
+def get_cartesian_coordinates(triangle_coordinates: np.ndarray, barycentric_coordinates: np.ndarray) -> np.ndarray:
+    """
+    Parameters:
+        triangle_coordinates: a 2x3 matrix representing triangle coordinates
+        barycentric_coordinates: a 1x3 matrix representing lambdas
+
+    Return:
+        a 1-d array representing (x,y)
+    """
     x_y = np.matmul(barycentric_coordinates, triangle_coordinates)
     return x_y
 
