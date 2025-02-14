35d3a6bb64cae42a4356acc533f7b12243a2156d
lyangelina <angelinalyy27@gmail.com>
2025-02-14 13:04:34 -0500

    finished trapezoid function

--- riemann.py
+++ riemann.py
@@ -1,8 +1,8 @@
 import numpy as np
 def trapezoid(x_vals: np.ndarray, func: np.ufunc) -> float:
     """
-    Calculates area of trapezoids under curve using vertices (a,0), (a, f(a)), (b,0), and (b, f(b))
-    Area of trapezoid = (f(a) + f(b)) * (b-a)/2), where f(a) and f(b) are y values of the x vales, and b-a is height.
+    Calculates area of trapezoids under curve using vertices
+    Area of trapezoid = (f(a) + f(b)) * (b-a)/2), where f(a) and f(b) are y values of the x vales, and b-a is the width between x-values.
 
     Parameters:
         x_vals: an 1-d array of the x-values used in determining the integral
@@ -12,9 +12,9 @@ def trapezoid(x_vals: np.ndarray, func: np.ufunc) -> float:
         sum_of_trapezoids: a float that is the sum of all the trapezoids used to approximate the area
     """
     y_vals = func(x_vals)
-    height = np.
-    sum_of_trapezoids = np.sum
+    lengths_per_x_vals = y_vals[:-1] + y_vals[1:]
+    widths = x_vals[1:] - x_vals[:-1]
+    sum_of_trapezoids = np.sum(lengths_per_x_vals * widths)/2
 
     return sum_of_trapezoids
-
 
