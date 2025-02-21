e746c2bc5bda05082e7be5f79bbbe3da5b021fa3
lyangelina <angelinalyy27@gmail.com>
2025-02-20 22:14:06 -0500

    finished get_cartesian_coordinates function

--- barycentric.py
+++ barycentric.py
@@ -2,12 +2,15 @@ import numpy as np
 def get_cartesian_coordinates(triangle_coordinates: np.ndarray, barycentric_coordinates: np.ndarray) -> np.ndarray:
     """
     Parameters:
-        triangle_coordinates: a 2x3 matrix representing triangle coordinates
-        barycentric_coordinates: a 1x3 matrix representing lambdas
+        triangle_coordinates: a 2x3 matrix representing triangle coordinates in the shape
+                ([x1, x2, x3],
+                [y1, y2, y3])
+        barycentric_coordinates: a 1x3 matrix representing lambdas in the shape
+                ([lambda1,lambda2, lambda3])
 
     Return:
-        a 1-d array representing (x,y)
+        a 1-D array representing (x,y)
     """
-    x_y = np.matmul(barycentric_coordinates, triangle_coordinates)
+    x_y = np.matmul(triangle_coordinates, barycentric_coordinates)
     return x_y
 
