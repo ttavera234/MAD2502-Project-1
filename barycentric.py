cef9ee9e07b08b7e83e304e3dbec0ff02a3cbb16
Diego-PG <diegopenuelagonzalez@gmail.com>
2025-02-21 01:56:50 -0500

    Added "is_inside_triangle" function

--- barycentric.py
+++ barycentric.py
@@ -14,3 +14,21 @@ def get_cartesian_coordinates(triangle_coordinates: np.ndarray, barycentric_coor
     x_y = np.matmul(triangle_coordinates, barycentric_coordinates)
     return x_y
 
+def is_inside_triangle(triangle_coordinates: np.ndarray, point_coordinates) -> bool:
+    x1, y1 = triangle_coordinates[:,0]
+    x2, y2 = triangle_coordinates[:,1]
+    x3, y3 = triangle_coordinates[:,2]
+    px, py = point_coordinates
+
+    def dire(x1, y1, x2, y2, x3, y3):
+        return (x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3)
+
+    d1 = dire(px, py, x1, y1, x2, y2)
+    d2 = dire(px, py, x2, y2, x3, y3)
+    d3 = dire(px, py, x3, y3, x1, y1)
+    if (d1 <= 0) and (d2 <= 0) and (d3 <= 0):
+        return True
+    elif (d1 >= 0) and (d2 >= 0) and (d3 >= 0):
+        return True
+    else:
+        return False
